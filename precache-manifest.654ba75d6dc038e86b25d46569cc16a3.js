24c73654e23451b451071b00b75960abd32356d5
Your Name <griseldavcardozo@gmail.com>
2019-06-04 19:43:09 -0300

    Updates

---
+++ precache-manifest.654ba75d6dc038e86b25d46569cc16a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6d27aff2ce8d34d902d",
+    "url": "/store-recording/static/css/main.42d69715.chunk.css"
+  },
+  {
+    "revision": "b6d27aff2ce8d34d902d",
+    "url": "/store-recording/static/js/main.b6d27aff.chunk.js"
+  },
+  {
+    "revision": "732749cf90fd78e223d2",
+    "url": "/store-recording/static/css/1.f093e5ea.chunk.css"
+  },
+  {
+    "revision": "732749cf90fd78e223d2",
+    "url": "/store-recording/static/js/1.732749cf.chunk.js"
+  },
+  {
+    "revision": "63816b8788c1b5d4b5e9",
+    "url": "/store-recording/static/js/runtime~main.63816b87.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/store-recording/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "06579dfed31c861941941dd5c3826c5d",
+    "url": "/store-recording/index.html"
+  }
+];
